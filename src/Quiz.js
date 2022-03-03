08c06e97e26907e3297e9a16338338669ba2b767
deepak choudhary <deepak32145@gmail.com>
2022-03-03 18:30:08 +0530

    adding quiz component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json');
 
@@ -9,8 +10,17 @@ class Quiz extends Component {
         this.state = {quiz_position : 1};
     }
     render(){
+        const isQuizEnd  = false;
+         if(this.state.quiz_position-1 === quizData.quiz_questions.length){
+            isQuizEnd = true;
+         }
+
         return (
             <div>
+                if(isQuizEnd){
+              <QuizEnd />
+                }
+              
             <QuizQuestion quiz_question ={quizData.quiz_questions[this.state.quiz_position-1]} />
             </div>
         )
